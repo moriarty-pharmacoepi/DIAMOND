f2bb962b69d71fd41c7505c6b7b1d31fb3db0e9f
Ryan Muddiman <ryanmuddiman@rcsi.com>
2024-10-09 14:39:07 +0100

    generating baseline covariates

--- Code/simulation.R
+++ Code/simulation.R
@@ -1,2 +1,29 @@
-# a test R script
+# Load the required package
 library(usethis)
+library("tidyverse")
+
+# Function for generating patient baseline covariates
+generate_random_covariates <- function() {
+  covariate_names <- c("Age", "smoker", "disease")
+  covariate_value<- c(runif(1, 18, 35), sample(0:1, 1), sample(0:1, 1))
+  # transpose the dataframe
+  covariates = t(data.frame(covariate_value))
+  colnames(covariates) <- covariate_names
+  
+  return(covariates)
+}
+
+# Constructor for patient observation
+new_Patient <- function(id) {
+  covariates <- generate_random_covariates()  # Generate random covariates
+  patient <- structure(list(id = id, init_covariates = covariates), class = "Patient")
+  return(patient)
+}
+
+# Patient population generation
+Patient_pop <- list()
+for (x in 1:1) {
+  Patient_pop[[x]] <- new_Patient(x)  # Assign a unique ID to each patient
+}
+print(Patient_pop[1])
+
