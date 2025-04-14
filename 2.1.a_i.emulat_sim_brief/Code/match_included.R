d8b3db70c000b6f729b21e79292a195336805b42
Ryan Muddiman <ryanmuddiman@rcsi.ie>
2025-04-14 16:03:59 +0100

    Add files via upload

--- 2.1.a_i.emulat_sim_brief/Code/match_included.R
+++ 2.1.a_i.emulat_sim_brief/Code/match_included.R
@@ -8,7 +8,7 @@ print(script_path)
 
 # Set the working directory to the script's location
 setwd(dirname(script_path))
-included_data_filename <- "review_522747_included_csv_20250110231400.csv"
+included_data_filename <- "review_522747_included_csv_20250411194606.csv"
 
 # read in csv file of included data from Covidence
 included_data <- read.csv(included_data_filename)
@@ -28,6 +28,7 @@ results_muddiman <- read.csv('all_pubs.csv')
 included_partial_1 <-merge(x=included_pmids,y=results_tazare,by="pmid")
 included_partial_1 <- included_partial_1[, c("pmid", "doi", "TITLE","year","article_type_pds")]
 colnames(included_partial_1) <-c("pmid", "doi", "title","year","publication_types")
+Tazare_sims <- results_tazare[results_tazare["simulation"]=="Yes",]
 
 #Data from new search included by Muddiman
 included_partial_2 <-merge(x=included_pmids,y=results_muddiman,by="pmid")
@@ -40,9 +41,23 @@ included_final <-rbind(included_partial_1,included_partial_2)
 included_final <- included_final %>%
   mutate(publication_types = ifelse(publication_types == "Original Article", "Journal Article", publication_types))
 
-# there is one mislabeled article, labelled as meta-analysis but its an article
+# this is a mislabeled article, labelled as meta-analysis but its an article
 included_final <- included_final %>%
   mutate(publication_types = ifelse(publication_types == "Meta-Analysis", "Journal Article", publication_types))
 
+# this is a mislabeled article, labelled as article but its a brief report
+included_final <- included_final %>%
+  mutate(publication_types = ifelse(pmid == 30375122, "Brief report", publication_types))
+
+# this is a mislabeled article, labelled as article but its a brief report
+included_final <- included_final %>%
+  mutate(publication_types = ifelse(pmid == 38450934, "Brief report", publication_types))
+
+# get the excluded articles from Tazare (screening and full-text)
+excluded_tazare <- anti_join(y=included_partial_1,x=Tazare_sims, by = c("pmid"))
+
+# get the excluded articles from Muddiman (screening and full-text)
+excluded_muddiman <- anti_join(y=included_partial_2,x=results_muddiman, by = c("pmid"))
+
 # Save included data to a CSV file
-write_csv(included_final, "included_final.csv")
+#write_csv(included_final, "included_final.csv")
